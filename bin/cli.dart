be315bd55230570e604088cc1efb10dc5f4ffe37
NWChen <neilwchen@gmail.com>
2024-10-19 23:32:43 -0400

    update code rendering

--- bin/cli.dart
+++ bin/cli.dart
@@ -7,6 +7,14 @@ import 'package:intl/intl.dart';
 
 final frontmatterRegex = RegExp(r'^---\n(.*)\n---', multiLine: true, dotAll: true);
 
+class MarkdownDocument {
+  final String title;
+  final DateTime date;
+  final String body;
+
+  MarkdownDocument({required this.title, required this.date, required this.body});
+}
+
 void main(List<String> arguments) async {
   if (arguments.isEmpty) {
     print('No arguments provided. Please provide markdown file paths.');
@@ -26,7 +34,7 @@ void main(List<String> arguments) async {
       final contents = await file.readAsString();
 
       // Convert markdown to HTML
-      final html = assembleHtmlWithFrontmatter(contents);
+      final html = toHtml(parseMarkdown(contents));
 
       // Generate output file path
       final fileName = path.basenameWithoutExtension(filePath);
@@ -42,40 +50,82 @@ void main(List<String> arguments) async {
   }
 }
 
-String prefixSrcReferences(String html) {
-  final srcRegex = RegExp(r'src="(/[^"]*)"');
-  return html.replaceAllMapped(srcRegex, (match) {
-    final originalPath = match.group(1);
-    return 'src="..$originalPath"';
-  });
-}
-
-String assembleHtmlWithFrontmatter(String markdown) {
-  final frontmatter = parseFrontmatter(markdown);
-  final body = markdown.replaceFirst(frontmatterRegex, '').trim();
-  String bodyHtml = markdownToHtml(body);
-  bodyHtml = prefixSrcReferences(bodyHtml);
+String toHtml(MarkdownDocument document) {
   final dateFormatter = DateFormat('MMMM d, yyyy');
+  var bodyHtml = markdownToHtml(document.body);
+  bodyHtml = prefixRefs(bodyHtml);
 
   return '''
-  <h1>
-  ${frontmatter['title']}
-  </h1>
-  <i>${dateFormatter.format(frontmatter['date'])}</i>
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <link rel="stylesheet" href="../css/styles.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+      <script>hljs.highlightAll();</script>
+    </head>
 
-  $bodyHtml
+    <body>
+      <h1>${document.title}</h1>
+      <i>${dateFormatter.format(document.date)}</i>
+      ${bodyHtml}
+    </body>
+  </html>
   ''';
 }
 
+// 1. extract frontmatter and rest from markdown
+// 2. prefix src references
+// 3. assemble html
+MarkdownDocument parseMarkdown(String markdown) {
+  final frontmatter = parseFrontmatter(markdown);
+  final body = markdown.replaceFirst(frontmatterRegex, '').trim();
+
+  return MarkdownDocument(
+    title: frontmatter['title'],
+    date: frontmatter['date'],
+    body: body
+  );
+}
+
+String prefixRefs(String html) {
+  final regex = RegExp(r'(src|href)="(/[^"]*)"');
+  return html.replaceAllMapped(regex, (match) {
+    final attribute = match.group(1);
+    final originalPath = match.group(2)!;
+    return '$attribute="..$originalPath"';
+  });
+}
+
+// String prefixSrcReferences(String html) {
+//   final srcRegex = RegExp(r'src="(/[^"]*)"');
+//   return html.replaceAllMapped(srcRegex, (match) {
+//     final originalPath = match.group(1);
+//     return 'src="..$originalPath"';
+//   });
+// }
+
+// String assembleHtmlWithFrontmatter(String markdown) {
+//   final frontmatter = parseFrontmatter(markdown);
+//   final body = markdown.replaceFirst(frontmatterRegex, '').trim();
+//   String bodyHtml = markdownToHtml(body);
+//   bodyHtml = prefixSrcReferences(bodyHtml);
+//   final dateFormatter = DateFormat('MMMM d, yyyy');
+
+//   return '''
+//   <h1>
+//   ${frontmatter['title']}
+//   </h1>
+//   <i>${dateFormatter.format(frontmatter['date'])}</i>
+
+//   $bodyHtml
+//   ''';
+// }
+
 Map<String, dynamic> parseFrontmatter(String markdown) {
   final match = frontmatterRegex.firstMatch(markdown);
-
-  if (match == null) {
-    return {};
-  }
-
-  final frontmatterYaml = match.group(1) ?? '';
-  final parsedYaml = loadYaml(frontmatterYaml);
+  final frontmatterYaml = match?.group(1) ?? '';
+  final parsedYaml = loadYaml(frontmatterYaml) as Map;
 
   String dateString = parsedYaml['date'].toString();
   DateTime date = DateTime.parse(dateString);
